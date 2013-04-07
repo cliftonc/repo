5d2b5abfc8d7475982148c1697010fff9f4d5ec3
cliftonc <clifton.cunningham@gmail.com>
2013-04-07 20:01:48 +0100

    Major rework

--- client.js
+++ client.js
@@ -1,14 +1,31 @@
 #!/usr/bin/env node
 
-var optimist = require('optimist')
-  , command = require('./lib/command');
+var nconf = require('nconf')
+  , optimist = require('optimist')
+  , fs = require('fs')
+  , path = require('path')
+  , util = require('util')
+  , command = require('./lib/command')
+  , watch = require('./lib/watch');
 
-var argv = optimist
+// Configure nconf
+nconf.argv()
+     .file({ file: __dirname + '/conf/' + (process.env.NODE_ENV || 'development') + '.json' });
+
+var url = "http://" + nconf.get('host') + ":" + nconf.get('port')
+  , externalUrl = "http://" + nconf.get('externalUrl')
+  , argv = optimist
     .usage(command.help())
-    .default({ u: 'http://127.0.0.1:8081'})
-    .describe('u', 'The base URL of the repo server (e.g. http://repo.dailymail.co.uk:8080)')
+    .default({ u: externalUrl, a: false})
+    .describe('u', 'The base URL of the repo server (e.g. http://repo.dailymail.co.uk)')
     .alias('u', 'url')
-    .demand(['u'])
+    .demand(['u'])    
+    .describe('a', 'Watch for changes and automatically publish this version to the repository.')
+    .alias('a', 'auto')    
+    .boolean('a')
+    .describe('f', 'Force publish over the top of live versions without warning or confirmation - be v. careful.')
+    .alias('f', 'force')    
+    .boolean('f')
     .argv;
 
 if (argv.h) {
@@ -16,10 +33,27 @@ if (argv.h) {
   process.exit(0);
 }
 
+
 // Convert the arguments into commands and create a client command object
 var commandName = argv._[0],    
-    command = command.init({url: argv.url, params: argv._.slice(1,argv._.length)});
+    command = command.init({url: externalUrl, force: argv.force, params: argv._.slice(1,argv._.length)});
 
 // Execute the command if it exists.
-command[commandName] ? command[commandName]() : optimist.showHelp();
+if(argv.auto && commandName == 'publish') {  
 
+ if(argv.force) return watch(function() { command['publish']() }); 
+
+ // First we need to check if this version is the current live version
+ command.isLiveVersion(function(isLiveVersion, versionInfo) {
+
+    if(isLiveVersion) return console.log("WARNING".red + " you cannot automatically publish changes to the current live version, please create a new version or publish manually.")
+    watch(function() { command['publish']() }); 
+
+ });
+ 
+
+} else {
+
+  command[commandName] ? command[commandName]() : optimist.showHelp();
+
+}
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
-var restify = require('restify')
+var nconf = require('nconf')
+  , restify = require('restify')
   , fs = require('fs')
   , tar = require('tar')
   , zlib = require('zlib')
@@ -8,21 +9,40 @@ var restify = require('restify')
   , rimraf = require('rimraf')
   , mkdirp = require('mkdirp')
   , semver = require('semver')
-  , optimist = require('optimist');
+  , Mustache = require('mustache')
+  , optimist = require('optimist')
+  , _ = require('lodash')
+  , winston = require('winston')
+  , socketio = require('socket.io');
+
+// Configure nconf
+nconf.argv()
+     .file({ file: './conf/' + (process.env.NODE_ENV || 'development') + '.json' });
+
+// Configure winston
+var log = new (winston.Logger)({
+  transports: [
+    new (winston.transports.Console)({ colorize: 'true', level: nconf.get('loglevel')}),
+    new (winston.transports.File)({ filename: './logs/server.log', level: nconf.get('loglevel')})
+  ]
+});
 
 // ----------------------------------------------------------------------------
 // options parsing
 // ----------------------------------------------------------------------------
 
-var argv = optimist
-    .usage('Number 5 is alive!\nUsage: $0')
-    //.demand(['d'])
-    .default({ d : path.join(process.cwd(), 'data'), p : 8080 })
+var url = "http://" + nconf.get('host') + ":" + nconf.get('port'),
+    externalUrl = "http://" + nconf.get('externalUrl'),
+    argv = optimist
+    .usage('Number 5 is alive!\nUsage: $0')    
+    .default({ d : path.join(process.cwd(), 'data'), p : nconf.port, s: nconf.host })
     .alias('d', 'data')
     .alias('p', 'port')
-    .alias('h', 'help')
+    .alias('h', 'help')    
+    .alias('s', 'servername')
     .describe('d', 'Directory to store template data')
     .describe('p', 'What port should I listen on?')
+    .describe('s', 'What is my server name?')
     .argv;
 
 if (argv.h) {
@@ -32,152 +52,274 @@ if (argv.h) {
 
 
 // ----------------------------------------------------------------------------
-// data initialization
-// ----------------------------------------------------------------------------
-
-var Data = require('./lib/data');
-var data = new Data({ dataDirectory: argv.data });
-data.init(function (err) {
-  console.log("Starting to load templates in " + data._packagesDir);
-  data.reloadPackages(function (err) {
-    if (err) throw err;
-    console.log("Done loading packages")
-  });
-});
-
-// ----------------------------------------------------------------------------
 // server wireup
 // ----------------------------------------------------------------------------
 
 var server = restify.createServer();
+server.use(restify.queryParser());
 
-server.get('/', function (req, res) {
-  res.send('Number 5 is alive')
+
+// ----------------------------------------------------------------------------
+// simple web sockets
+// ----------------------------------------------------------------------------
+
+var io = socketio.listen(server), sockets = [];
+io.set('log level', 1);
+io.sockets.on('connection', function (socket) {
+  sockets.push(socket);
 });
 
-server.put('/package/:name/:version', function (req, res, next) {
+var notifyPackage = function(data) {
+  _.map(sockets, function(socket) {
+      socket.emit('package', data);
+  });
+}
+
+// ----------------------------------------------------------------------------
+// data initialization
+// ----------------------------------------------------------------------------
+
+var Data = require('./lib/data');
+var Combinator = require('./lib/combinator');
+
+var data = new Data({ dataDirectory: argv.data, url: externalUrl, notify: notifyPackage, sortOrder: nconf.get('sortOrder'), log: log}), combinator;
+
+data.init(function (err) {
+  data.reloadPackages(function (err) {
+    if (err) throw err;
+    afterInitialised();
+  });
+});
+
+function afterInitialised() {  
+  
+  // Setup the combinator
+  combinator = new Combinator(data, {url: externalUrl, log: log}); 
+
+  // Start the server
+  server.listen(nconf.get('port'), nconf.get('host'), function() {  
+    console.log('\r\n' + require('./lib/logo').join('\r\n') + '\r\n');  
+    console.log('Repo listening at ' + url.green + ' [' + externalUrl.blue + ']');
+  });
+
+}
+
+server.get(/\/ui\/?.*/, restify.serveStatic({
+  directory: '.'
+}));
+
+server.get('/', function (req, res, next) {
+  res.statusCode = 301;
+  res.header('Location', '/ui/index.html');
+  res.end();  
+});
+
+server.put('/api/package/:name/:version', function (req, res, next) {
 
   var name = req.params.name;
   var version = req.params.version;
   var rand = Math.floor(Math.random()*4294967296).toString(36);
   var tempPackageFile = path.join(argv.data, "temp", rand + name + "-" + version + ".tgz");
 
+  log.debug('Start upload for package ' + name + ' @ ' + version);
+
   // write the tar file. Don't combine the streamed gzip and untar on upload just yet...
   req
     .on('end', function () { 
       data.addPackage(tempPackageFile, name, version, function (err) {
         if (err) {
-          console.error("Error adding package from upload: " + (err.message || err));
+          log.error("Error adding package from upload: " + (err.message || err));
           fs.unlink(tempPackageFile);
           return next(err);
         }
         fs.unlink(tempPackageFile);
+        log.debug('Completed upload for package ' + name + ' @ ' + version);
         res.send(200);
       });
     })
-    .pipe(fs.createWriteStream(tempPackageFile))
     .on('error', function (err) {
-      console.error("Unexpected error when accepting package upload: " + (err.message || err));
+      log.error("Unexpected error when accepting package upload: " + (err.message || err));
       fs.unlink(tempPackageFile);
       res.send(err, 500);
-    });
-
+    })
+    .pipe(fs.createWriteStream(tempPackageFile));
+    
 });
 
-server.del('/package/:name/:version', function (req, res, next) {
+server.del('/api/package/:name/:version', function (req, res, next) {
   var name = req.params.name;
   var version = req.params.version;
+  log.debug('DELETED package ' + name + ' @ ' + version);
   data.deletePackage(name, version, function (err) {
     if (err) {
-      console.error("Error deleting package " + name + "@" + version + ": " + (err.message || err));
+      log.error("Error deleting package " + name + "@" + version + ": " + (err.message || err));
       return next(err);
     }
     res.send(200);
   });
 });
 
-server.get('/versions/:name', function (req, res) {
+server.get('/api/versions/:name', function (req, res) {
   var name = req.params.name;
+  log.debug('Requested version list for ' + name);
   res.send(data.whichVersions(name));
 });
 
-server.get('/package/:name/:range', function (req, res, next) {
-  var name = req.params.name;
-  var range = req.params.range;
-  if (range === 'latest') 
-    range = 'x.x.x';
-  returnPackageByRange(name, range, res);
-});
 
-server.get('/index', function (req, res) {
+server.get('/api/index', function (req, res) {
+  log.debug('Index for all packages'); 
   res.send(data.index());
 });
 
-server.get('/index/:type', function (req, res) {  
+server.get('/api/index/type/:type', function (req, res) { 
+  log.debug('Index by type for ' + req.params.type); 
   res.send(data.indexByType(req.params.type));
 });
 
-server.get('/info/:name', function (req, res) {
+server.get('/api/index/search/:search', function (req, res) {  
+  log.debug('Index search for ' + req.params.search);
+  res.send(data.indexBySearch(req.params.search));
+});
+
+server.get('/api/info/:name', function (req, res) {
   var name = req.params.name;  
   var meta = data.packageMeta(name);
+  log.debug('Requested info for ' + name);
   if (!meta) return res.send(404);
   else return res.send(meta);
 });
 
-server.listen(argv.port, function() {
-  console.log('Repo listening at %s', server.url);
+server.get('/api/package/:name/:version', function (req, res) {
+
+  var name = req.params.name;
+  var version = (req.params.version == 'latest' ? data._findLatest(name) : req.params.version);
+  log.debug('Downloaded package ' + name + ' @ ' + version );
+  returnPackage(name, version, res);
+  
 });
 
-// ----------------------------------------------------------------------------
-// register permutations of gt,lt,gte,lte routes for semver magic 
-// ----------------------------------------------------------------------------
+server.get('/js/:key', function (req, res) {
 
-var ops = [['gt', '>'], ['lt', '<'], ['gte', '>='], ['lte', '<=']]
+  var key = req.params.key, 
+      latest = req.params.latest;
 
-ops.forEach(function (op1) {
-  //console.log (op1);
-  registerOp(op1);
-  ops.forEach(function (op2) {
-    if (op1 != op2) {
-      //console.log(op1, op2);
-      registerOp(op1, op2);
-    }
+  log.debug('JS ' + key + ' & ' + (latest ? 'latest' : live));
+
+  res.contentType = 'application/javascript'; // Lookup  
+  combinator.compressJs(key, latest ? 'latest' : 'live', function(err, js) {
+    if(err) return res.send(err);
+    res.end(js.code);  
+  });
+
+});
+
+server.get('/css/:key', function (req, res) {
+  
+  var key = req.params.key, 
+      latest = req.params.latest;
+
+  log.debug('CSS ' + key + ' & ' + (latest ? 'latest' : live));
+
+  res.contentType = 'application/css'; // Lookup  
+  combinator.compressCss(key, latest ? 'latest' : 'live', function(err, css) {
+    if(err) return res.send(err);
+    res.end(css);
+  }); 
+  
+});
+
+server.get('/jsmap/:key', function (req, res) {
+  var key = req.params.key,
+      latest = req.params.latest;
+
+  log.debug('JS Source Map ' + key + ' & ' + (latest ? 'latest' : live));
+
+  res.contentType = 'application/javascript'; // Lookup
+  combinator.compressJs(key, latest ? 'latest' : 'live', function(err, js) {
+    if(err) return res.send(err);
+    res.end(js.map);  
+  });
+
+});
+
+server.get(/^\/(src)\/([a-zA-Z0-9_\.~-]+)\/([0-9_\.~-]+)\/(.*)/, function (req, res) {
+
+  var name = req.params[1];
+  var version = req.params[2];
+  var asset = req.params[3];
+
+  log.debug('Request for asset ' + name + ' @ ' + version + ': '  + asset);
+
+  returnPackageAsset(name, version, asset, res);
+  
+});
+
+server.get('/preview/:name', function(req, res) {
+
+  var name = req.params.name;
+  var version = data._findLatest(name);
+  var template = req.params.name;
+
+  log.debug('Showing ' + name + ' @ ' + version + ' using '  + template + ' template');
+  
+  var top = Mustache.compile(fs.readFileSync('./templates/top.html').toString())({url: externalUrl, name: name, version: version}),
+      bottom = Mustache.compile(fs.readFileSync('./templates/bottom.html').toString())({url: externalUrl, name: name, version: version});
+
+  data.renderPackage(name, version, template, function(err, html) {
+    if(err) return res.end(err.message);
+    res.end(top + html + bottom)    
   })
+
 })
 
-function registerOp (op1, op2) {
-  if (!op2) {
-    //console.log('/package/:name/' + op1[0] + '/:v1')
-    server.get('/package/:name/' + op1[0] + '/:v1', function (req, res, next) {
-      var name = req.params.name;
-      var v1 = req.params.v1;
-      var range = op1[1] + v1;
-      returnPackageByRange(name, range, res);
-    });    
-  }
-  else {
-    //console.log('/package/:name/' + op1[0] + '/:v1/' + op2[0] + '/:v2')
-    server.get('/package/:name/' + op1[0] + '/:v1/' + op2[0] + '/:v2', function (req, res, next) {
-      var name = req.params.name;
-      var v1 = req.params.v1;
-      var v2 = req.params.v2;
-      var range = op1[1] + v1 + ' ' + op2[1] + v2;
-      returnPackageByRange(name, range, res);
-    });    
+server.get('/preview/:name/:version/:template', function(req, res) {
 
-  }
+  var name = req.params.name;
+  var version = req.params.version;
+  var template = req.params.template;
+
+  log.debug('Showing ' + name + ' @ ' + version + ' using '  + template + ' template');
+  
+  var top = Mustache.compile(fs.readFileSync('./html/top.html').toString())({url: externalUrl, name: name, version: version}),
+    bottom = Mustache.compile(fs.readFileSync('./html/bottom.html').toString())({url: externalUrl, name: name, version: version});
+
+  data.renderPackage(name, version, template, function(err, html) {
+    if(err) return res.end(err.message);
+    res.end(top + html + bottom)
+  });
+
+})
+
+
+function returnPackageAsset (name, version, asset, res) {
+  
+  var filename = path.basename(asset);
+
+  res.contentType = 'application/x-compressed'; // Lookup
+  res.header( "Content-Disposition", "filename=" + filename );
+
+  data.openAssetStream(name, version, asset, function (err, stream) {
+    if (err) {
+      console.error("Error streaming asset: " + (err.message || err));
+      res.send(err, 500);
+    }
+    stream
+      .pipe(res)
+      .on('error', function (err) {
+        console.dir(err);
+        res.send(err, 500);
+      });
+  })
+
 }
 
-function returnPackageByRange (name, range, res) {
-  var version = semver.maxSatisfying(data.whichVersions(name), range);
-  console.log("semver range calculation of (" + name, range + ")  ==> ", version);
 
-  if (!version) { 
-    return res.send(404) 
-  }
 
+function returnPackage (name, version, res) {
+  
   var filename = name + '-' + version + '.tgz';
-  res.contentType = 'application/x-compressed';
+
+  res.contentType = 'application/x-compressed'; // Lookup
   res.header( "Content-Disposition", "filename=" + filename );
 
   data.openPackageStream(name, version, function (err, stream) {
@@ -188,9 +330,9 @@ function returnPackageByRange (name, range, res) {
     stream
       .pipe(res)
       .on('error', function (err) {
+        console.dir(err);
         res.send(err, 500);
       });
   })
+
 }
-
-
